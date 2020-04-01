c390a96bc4709d33134eea1a46b2f6956f6cb211
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-01 19:29:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,8 @@ def add_element_to_end_of_array(array, element)
   array << "arrays!"
 end
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
